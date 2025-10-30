17b3ac9d76559e3b705c03587922bd2b29daa3be
Masudur400 <md8004210@gmail.com>
2025-10-30 14:47:18 +0600

    login component ready

--- src/components/modules/Home/Specialties.tsx
+++ src/components/modules/Home/Specialties.tsx
@@ -31,7 +31,7 @@ const specialists = [
 
 const Specialities = () => {
   return (
-    <section className="py-24 mt-24 md:mt-20">
+    <section className="py-10 mt-10 ">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8">
           <div>
