71448bde36c135ea1bea5628611d4befb0144a5d
Masudur400 <md8004210@gmail.com>
2025-11-02 18:21:58 +0600

    get accesstoken by refreshtoken

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,43 +1,102 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { jwtDecode } from "jwt-decode"; 
+import { userInterface } from "./types/userTypes";
 
-    const token = request.cookies.get('accessToken')?.value;
 
-    console.log(token)
 
-    const { pathname } = request.nextUrl;
+const roleBasedRoutes = {
+  ADMIN: ["/admin/dashboard",],
+  DOCTOR: ["/doctor/dashboard"],
+  PATIENT: [
+    "/patient/dashboard",
+    "/patient/appointments",
+    "/patient/medical-records",
+  ],
+};
 
-    const protectedPaths = ['/dashboard/*', '/profile', '/settings', '/appointments'];
+const authRoutes = ["/login", "/register", "/forgot-password"];
 
-    const authRoutes = ['/login', '/register', '/forgot-password'];
+export async function proxy(request: NextRequest) {
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
 
-    const isProtectedPath = protectedPaths.some((path) => {
-        pathname.startsWith(path);
-    })
+  const { pathname } = request.nextUrl;
 
-    // current path auth route or not
-    const isAuthRoute = authRoutes.some((route)=>
-        pathname === route
-    )
+  if (!accessToken && !refreshToken && !authRoutes.includes(pathname)) {
+    return NextResponse.redirect(
+      new URL(`/login?redirect=${pathname}`, request.url)
+    );
+  }
 
-    console.log(isAuthRoute)
+  let user: userInterface | null = null;
 
-    if(isProtectedPath && !token){
-        return NextResponse.redirect(new URL('/login', request.url))
+  if (accessToken) {
+    try {
+      user = jwtDecode(accessToken); // {id: string, email: string, role: "ADMIN"| "DOCTOR" | "PATIENT", exp: number, iat: number}
+    } catch (err) {
+      console.log("Error decoding access token:", err);
+      return NextResponse.redirect(
+        new URL(`/login?redirect=${pathname}`, request.url)
+      );
     }
+  }
 
-    if(isAuthRoute && token){
-        console.log('hitting the correct block')
-        return NextResponse.redirect(new URL('/', request.url))
+  if (!user && refreshToken) {
+    try {
+      const refreshRes = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh-token`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ refreshToken }),
+        }
+      );
+      if (refreshRes.ok) {
+        const newAccessToken = request.cookies.get("accessToken")?.value;
+        user = jwtDecode(newAccessToken!);
+        return NextResponse.next();
+      } else {
+        const response = NextResponse.redirect(
+          new URL(`/login?redirect=${pathname}`, request.url)
+        );
+        response.cookies.delete("accessToken");
+        response.cookies.delete("refreshToken");
+        return response;
+      }
+    } catch (err) {
+      console.log("Error refreshing token:", err);
+      const response = NextResponse.redirect(
+        new URL(`/login?redirect=${pathname}`, request.url)
+      );
+      response.cookies.delete("accessToken");
+      response.cookies.delete("refreshToken");
+      return response;
     }
+  }
 
-    return NextResponse.next()
+
+   if(user){
+    const allowedRoutes = user ? roleBasedRoutes[user.role] : [];
+    if(allowedRoutes && allowedRoutes.some((r)=>pathname.startsWith(r))){
+        return NextResponse.next();
+    }else{
+        return NextResponse.redirect(new URL(`/unauthorized`, request.url));
+    }
+   }
+
+   if(user && authRoutes.includes(pathname)){
+    return NextResponse.redirect(new URL(`/`));
+   }
+   
+
+
+  return NextResponse.next();
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard/:path*', '/login', '/register', '/forgot-password'],
-}
+  matcher: ["/admin/dashboard/:path*", "/login", "/register", "/forgot-password"],
+};
