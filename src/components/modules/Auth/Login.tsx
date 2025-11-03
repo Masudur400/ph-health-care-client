89b06eb8f3a0bdcbe7f30e585e0b7d72261921d0
Masudur400 <md8004210@gmail.com>
2025-11-03 16:42:48 +0600

    logout function implimenting

--- src/components/modules/Auth/Login.tsx
+++ src/components/modules/Auth/Login.tsx
@@ -24,10 +24,11 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"; 
+} from "@/components/ui/form";
+import loginUser from "@/utility/login";
 import { useRouter } from "next/navigation";
 import checkAuthStatus from "@/utility/auth";
-import loginUser from "@/utility/login";
+import { UseUser } from "@/Providers/UserProvider";
 
 const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -43,6 +44,7 @@ export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
+  const {setUser} = UseUser();
 
   const form = useForm<LoginFormData>({
     resolver: zodResolver(loginSchema),
@@ -63,13 +65,15 @@ export default function Login() {
 
       if (res.success) {
         const authStatus = await checkAuthStatus();
+        
+         setUser(authStatus.user); //setUser Immiditaly.
 
         if(authStatus.isAuthenticated && authStatus.user){
           const {role} = authStatus.user;
 
           switch(role){
             case "ADMIN":
-              router.push("/dashboard/admin");
+              router.push("/admin/dashboard");
               break;
             case "DOCTOR":
               router.push("/dashboard/doctor");
