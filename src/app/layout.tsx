7ce27de89270527771e0f462187e8a247ab6b862
Masudur400 <md8004210@gmail.com>
2025-11-09 20:41:49 +0600

    delete unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { UserProvider } from "@/Providers/UserProvider";
+import "./globals.css"; 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,9 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <UserProvider>
-            {children}
-        </UserProvider>
+    
+            {children} 
       </body>
     </html>
   );
